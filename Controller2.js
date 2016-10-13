ff9f1b3f3d8f7a0e67a14e5a6ccd0355ec9ef930
Happyplankton <lzuenrauri@users.noreply.github.com>
2016-10-13 10:37:17 +0800

    use  to analyse an expression c6.1

---
+++ Controller2.js
@@ -0,0 +1,9 @@
+angular.module("myApp", []).controller('MyController' ,
+	function($scope, $parse) {
+		$scope.$watch('expr', function(newVal, oldVal, scope) {
+			if(newVal!=oldVal) {
+				var parseFun = $parse(newVal);
+				$scope.parseValue = parseFun(scope);
+			}
+		});
+	});
