21e26046ff7c4331f5561b9a8a22ba66e2c2a0b2
Happyplankton <lzuenrauri@users.noreply.github.com>
2016-10-13 11:29:15 +0800

    Still cannot run the demo...Error: MyController is not a function...Well, I copied the original code on the book...What's wrong?!

--- Controller2.js
+++ Controller2.js
@@ -1,9 +1,13 @@
-angular.module("myApp", []).controller('MyController' ,
-	function($scope, $parse) {
-		$scope.$watch('expr', function(newVal, oldVal, scope) {
-			if(newVal!=oldVal) {
-				var parseFun = $parse(newVal);
-				$scope.parseValue = parseFun(scope);
-			}
-		});
-	});
+function MyController($scope, $parse) {
+    $scope.person = {
+        name: "Ari Lerner"
+    };
+    $scope.$watch('expr', function(newVal, oldVal, scope) {
+        if (newVal !== oldVal) {
+          // Let's set up our parseFun with the expression
+          var parseFun = $parse(newVal);
+          // Get the value of the parsed expression, set it on the scope for output
+          scope.parsedExpr = parseFun(scope);
+        }
+    });
+};
